0e7d80f79e6f6cb4f5e6f8f44cdd317442942897
lucasmedrano <lucasmedrano97@gmail.com>
2016-12-04 00:16:34 -0300

    Update grafo.py

--- grafo.py
+++ grafo.py
@@ -1,11 +1,13 @@
 import random
 import math
+import heapq
 
 visitar_nulo = lambda a,b,c,d: True
 heuristica_nula = lambda actual,destino: 0
 
 class Personaje:
     def __init__(self, nombre, distancia = math.inf):
+        '''Nombre debe ser un string, y distancia un numero'''
         self.nombre = nombre
         self.distancia = distancia
 
@@ -19,11 +21,16 @@ class Personaje:
         return self.distancia > otro.distancia
 
     def cambiar_distancia(self, distancia_nueva):
+        '''Permite cambiar la distancia del personaje. distancia_nueva debe ser un numero'''
         self.distancia = distancia_nueva
+
     def obtener_nombre(self):
+        '''Devuelve el nombre de un personaje'''
         return self.nombre
-class Arista:
+
+class Arista
     def __init__(self, desde, hasta, peso):
+        '''desde y hasta deben ser vertices del grafo. peso debe ser un numero'''
         self.desde = desde
         self.hasta = hasta
         self.peso = peso
@@ -68,6 +75,10 @@ class Grafo():
         ''' Determina si el grafo contiene un vertice con el nombre indicado.'''
         '''Devuelve si lo contiene o no'''
         return(nombre in self.vertices.keys())
+
+    def obtener_cantidad_aristas(self):
+        '''Devuelve la cantidad de aristas del grafo'''
+        return self.cantidad_aristas
         
     def agregar_arista(self, desde, hasta, peso = 1):
         '''Agrega una arista que conecta los vertices indicados. Parametros:
@@ -75,13 +86,11 @@ class Grafo():
             - Peso: valor de peso que toma la conexion. Si no se indica, valdra 1.
             Si el grafo es no-dirigido, tambien agregara la arista reciproca.
         '''
-        #Si hasta o desde no pertenecen al grafo, se lanza un error
         if ((hasta or desde) not in self.vertices.keys()):
             raise KeyError("Alguno de los nombres ingresados no forma parte del grafo")
         
-        self.vertices[desde][hasta] = peso #Se cambia el peso de la arista
+        self.vertices[desde][hasta] = peso
 
-        #Si no es dirigido, se cambia tambien el peso de la arista inversa
         if not(self.es_dirigido):
             self.vertices[hasta][desde] = peso
         self.cantidad_aristas =+ 1;
@@ -92,19 +101,15 @@ class Grafo():
            En caso de no existir la arista, se lanzara ValueError.
            Si el grafo no es dirigido, se borrará la arista inversa.
         '''
-        
-        #Si desde o hasta no pertenecen al grafo, se lanza un KeyError
+
         if((desde or hasta) not in self.vetices.keys()):
             raise KeyError("Alguno de los nombres ingresados no forma parte del grafo")
 
-        #Si la relacion no existe, se lanza un ValueError
         if(hasta not in self.vertices[desde].keys()):
             raise ValueError("La arista ingresada por parámetro no existe")
 
-        #Elimino la arista Desde->Hasta
         self.vertices[desde].pop(hasta)
 
-        #Si no es dirigido, se borra también la arista Hasta->Desde
         if not(self.es_dirigido):
             self.vertices[hasta].pop(desde)
         self.cantidad_aristas =- 1;
@@ -130,40 +135,31 @@ class Grafo():
             raise KeyError("el nombre ingresado no forma parte del grafo")
         return self.vertices[nombre].keys()
     
-    #Recibe grafo, y tipo que indica si es BFS o DFS, luego llama a las funciones _BFS o _DFS segun corresponda.
-        def recorrido(grafo, tipo):
-        ''' Recorre el grafo y dependiendo del tipo que se le pase por parámetro lo recorre de forma BFS o DFS.
-            Devuelve dos diccionarios, uno llamado padre y otro orden que indican el orden de como se recorrio el grafo'''
-        visitados = {} #diccionario
+    def recorrido(self, tipo, inicio = None):
+        ''' Recorre el grafo y dependiendo del tipo (BFS o DFS) que se le pase por parámetro lo recorre de forma BFS o DFS.
+        Devuelve dos diccionarios, uno llamado padre y otro orden que permiten conocer el orden en el cual se recorrio el grafo, 
+        y los ordenes de los vertices'''
+        visitados = {}
         padre = {}
         orden = {}
-        for v in grafo:
-            if v not in visitados:
+        if not inicio:
+            for v in self.vertices.keys():
+                if v not in visitados:
+                    padre[v] = None
+                    orden[v] = 0
+                    if(tipo == "BFS"): self._bfs(v, visitados, padre, orden)
+                    if(tipo == "DFS"): self._dfs(v, visitados, padre, orden)
+        else:
+            for v in self.vertices.keys():
                 padre[v] = None
                 orden[v] = 0
-                if(tipo == "BFS"):
-                    _bfs(grafo, v, visitados, padre, orden)
-                if(tipo =="DFS"):
-                    _dfs(grafo, v, visitados, padre, orden)
+            if(tipo == "BFS"): self._bfs(inicio, visitados, padre, orden)
+            if(tipo == "DFS"): self._dfs(inicio, visitados, padre, orden)
         return padre, orden
 
-    def bfs(grafo):
-        return recorrido(grafo, "BFS")
-
-    def dfs(grafo):
-        return recorrido(grafo, "DFS")
-
-    def _bfs(self, inicio, visitados, padre, orden):
+    def bfs(self, inicio = None):
         '''Realiza un recorrido BFS dentro del grafo, aplicando la funcion pasada por parametro en cada vertice visitado.
         Parametros:
-            - visitar: una funcion cuya firma sea del tipo: 
-                    visitar(v, padre, orden, extra) -> Boolean
-                    Donde 'v' es el nombre del vertice actual, 
-                    'padre' el diccionario de padres actualizado hasta el momento,
-                    'orden' el diccionario de ordenes del recorrido actualizado hasta el momento, y 
-                    'extra' un parametro extra que puede utilizar la funcion (cualquier elemento adicional que pueda servirle a la funcion a aplicar). 
-                    La funcion aplicar devuelve True si se quiere continuar iterando, False en caso contrario.
-            - extra: el parametro extra que se le pasara a la funcion 'visitar'
             - inicio: identificador del vertice que se usa como inicio. Si se indica un vertice, el recorrido se comenzara en dicho vertice, 
             y solo se seguira hasta donde se pueda (no se seguira con los vertices que falten visitar)
         Salida:
@@ -171,13 +167,16 @@ class Grafo():
                 - 'padre' es un diccionario que indica para un nombre, cual es el nombre del vertice padre en el recorrido BFS (None si es el inicio)
                 - 'orden' es un diccionario que indica para un nombre, cual es su orden en el recorrido BFS
         '''
+        return self.recorrido("BFS", inicio)
 
+
+    def _bfs(self, inicio, visitados, padre, orden):
         q = []
-        q.append(origen)
-        visitados[origen] = True
+        q.append(inicio)
+        visitados[inicio] = True
         while(len(q) > 0):
-            v = lista.pop(0)
-            for w in grafo.adyacentes(v):
+            v = q.pop(0)
+            for w in self.adyacentes(v):
                 if w not in visitados:
                     visitados[w] = True
                     padre[w] = v
@@ -185,23 +184,19 @@ class Grafo():
                     q.append(w)
 
     
-    def _dfs(self, inicio, visitados, padre, orden):
+    def dfs(self, inicio):
         '''Realiza un recorrido DFS dentro del grafo, aplicando la funcion pasada por parametro en cada vertice visitado.
-        - visitar: una funcion cuya firma sea del tipo: 
-                    visitar(v, padre, orden, extra) -> Boolean
-                    Donde 'v' es el identificador del vertice actual, 
-                    'padre' el diccionario de padres actualizado hasta el momento,
-                    'orden' el diccionario de ordenes del recorrido actualizado hasta el momento, y 
-                    'extra' un parametro extra que puede utilizar la funcion (cualquier elemento adicional que pueda servirle a la funcion a aplicar). 
-                    La funcion aplicar devuelve True si se quiere continuar iterando, False en caso contrario.
-            - extra: el parametro extra que se le pasara a la funcion 'visitar'
+        Parametros:
             - inicio: identificador del vertice que se usa como inicio. Si se indica un vertice, el recorrido se comenzara en dicho vertice, 
             y solo se seguira hasta donde se pueda (no se seguira con los vertices que falten visitar)
         Salida:
             Tupla (padre, orden), donde :
-                - 'padre' es un diccionario que indica para un identificador, cual es el identificador del vertice padre en el recorrido DFS (None si es el inicio)
-                - 'orden' es un diccionario que indica para un identificador, cual es su orden en el recorrido DFS
+                - 'padre' es un diccionario que indica para un nombre, cual es el nombre del vertice padre en el recorrido DFS (None si es el inicio)
+                - 'orden' es un diccionario que indica para un nombre, cual es su orden en el recorrido DFS
         '''
+        return self.recorrido("DFS")
+
+    def _dfs(self, inicio, visitados, padre, orden):
         visitados[v] = True
         for w in grafo.adyacentes(v):
             if w not in visitados:
@@ -209,9 +204,9 @@ class Grafo():
                 orden[w] = orden[v] + 1
                 self._dfs(w, visitados, padre, orden)
         
-    def camino_minimo(self, origen, destino, heuristica=heuristica_nula):
-        '''Devuelve el recorrido minimo desde el origen hasta el destino, aplicando el algoritmo de Dijkstra, o bien
-        A* en caso que la heuristica no sea nula. Parametros:
+    def camino_minimo(self, origen, destino, inverso = False, heuristica=heuristica_nula):
+        '''Devuelve el recorrido minimo desde el origen hasta el destino, aplicando el algoritmo de Dijkstra. 
+        Parametros:
             - origen y destino: identificadores de vertices dentro del grafo. Si alguno de estos no existe dentro del grafo, lanzara KeyError.
             - heuristica: funcion que recibe dos parametros (un vertice y el destino) y nos devuelve la 'distancia' a tener en cuenta para ajustar los resultados y converger mas rapido.
             Por defecto, la funcion nula (devuelve 0 siempre)
@@ -224,43 +219,53 @@ class Grafo():
         padre = {}
         visitados = {}
 
-        for vertice in grafo.vertices.keys():
+        for vertice in self.vertices.keys():
             distancia[vertice] = math.inf
             padre[vertice] = None
-            visitado[vertice] = None
+            visitados[vertice] = False
 
-        personaje = Personaje(origen)
+        personaje = Personaje(origen, 0)
         distancia[origen] = 0
-        personaje.cambiar_distancia(0)
         heapq.heappush(heap, personaje)
 
         while(heap):
             personaje = heapq.heappop(heap)
             vertice = personaje.obtener_nombre()
             visitados[vertice] = True
-            for adyacente in grafo.adyacentes(vertice):
-                if not (vertice in visitados) and (distancia[adyacente] > (distancia[vertice] + grafo.obtener_peso_arista(vertice, adyacente))):
-                    distancia[adyacente] = distancia[vertice] + grafo.obtener_peso_arista(vertice, adyacente)
-                    padre[adyacente] = vertice
-                    personaje = Personaje(adyacente)
-                    heapq.heappush(heap, personaje)
 
-        camino_minimo = []
-        vertice = destino
-        camino_minimo.append(vertice)
-        while(vertice != origen):
-            camino_minimo.append(padre[vertice])
-            vertice = padre[vertice]
-        camino_minimo.reverse()
-        return camino_minimo
+            for adyacente in self.adyacentes(vertice):
+                if not inverso:
+                    if (visitados[adyacente] == False) and (distancia[adyacente] > (distancia[vertice] + self.obtener_peso_arista(vertice, adyacente))):
+                        distancia[adyacente] = distancia[vertice] + self.obtener_peso_arista(vertice, adyacente)
+                        padre[adyacente] = vertice
+                        personaje = Personaje(adyacente, distancia[adyacente])
+                        heapq.heappush(heap, personaje)
+                else:
+                    if (visitados[adyacente] == False) and (distancia[adyacente] > (distancia[vertice] + (1 / self.obtener_peso_arista(vertice, adyacente)))):
+                        distancia[adyacente] = (distancia[vertice]) + (1 / self.obtener_peso_arista(vertice, adyacente))
+                        padre[adyacente] = vertice
+                        personaje = Personaje(adyacente, distancia[adyacente])
+                        heapq.heappush(heap, personaje)
+        return self.descubrir_camino(origen, destino, padre, distancia)
 
-
-
-
+    def descubrir_camino(self, origen, destino, padre, distancia):
+        '''Parametros:
+            origen: vertice en el cual comienza el recorrido.
+            destino: vertice final del recorrido.
+            padre: un diccionario que indica el padre de cada vertice.
+            distancua: un diccionario con la distancia de cada vertice'''
+            camino_minimo = []
+            vertice = destino
+            camino_minimo.append(vertice)
+            while(vertice != origen):
+                camino_minimo.append(padre[vertice])
+                vertice = padre[vertice]
+            camino_minimo.reverse()
+            return camino_minimo
 
     def vertice_aleatorio(self, pesos):
-        ''' Devuelve un vértice aleatorio de los adyacentes del vertice que se recibe '''
-        #Pesos es un diccionario de pesos, clave vértice vecino, valor el peso. Acomodar a implementacion
+        ''' Devuelve un vértice aleatorio de los adyacentes del vertice que se recibe.
+        pesos es un diccionario que para cada arista, tiene su peso'''
         total = sum(pesos.values())
         rand = random.uniform(0, total)
         acum = 0
@@ -283,12 +288,12 @@ class Grafo():
         if not(origen):
             origen = random.choise(self.vertices.keys())
 
-        actual = origen #Creo auxiliar, que va a ser el actual que voy a ir corriento y agregando a la lista
+        actual = origen
         recorrido.append(actual)
-        if not(pesado): #No se tienen en cuenta los pesos. Simplemente se elije al azar
+        if not(pesado):
             for i in range(largo):
-                actual = random.choise(self.vertices[actual].keys()) #Elijo un vertice al azar entre todos los adyacentes
-                recorrido.append(actual) #Lo agrego a la lista
+                actual = random.choise(self.vertices[actual].keys())
+                recorrido.append(actual)
         if(pesado):
             for i in range(largo):
                 actual = self.vertice_aleatorio(self.vertices[actual])
